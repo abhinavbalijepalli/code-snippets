e8701b27e17280849d99b87b5cb86fcb1a85022e
abhinavbalijepalli <balijepalli.abhinav@gmail.com>
2025-09-02 01:17:58 -0700

    Create add.py

---
+++ add.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = int(input("Enter first integer: "))
+    b = int(input("Enter second integer: "))
+    print("Sum =", add(a, b))
